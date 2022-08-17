4e9b1e325f4dba11c0ef2a26cb68ab4367d62afa
Z1V3 <azivko@foi.hr>
2022-08-17 02:10:02 +0200

    dodan gumb i funkcija za izlaz iz aplikacije

--- Aplikacija_Git_Test/Aplikacija_Git_Test/Form1.cs
+++ Aplikacija_Git_Test/Aplikacija_Git_Test/Form1.cs
@@ -16,5 +16,10 @@ namespace Aplikacija_Git_Test
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
     }
 }
